a4aad49e16184ca6b810ef3b3045074ada49c0fe
rigo0523 <limon1285@gmail.com>
2020-05-15 21:51:55 -0500

    finished with Tabs component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,32 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+
+//tabsTopics is used to store all topics
+const tabTopics = document.querySelector('.topics');
+
+function newTopics(data) {
+    const newTab = document.createElement('div');
+    newTab.classList.add('tab');
+    newTab.textContent = data;
+
+    //append to topics class
+    tabTopics.append(newTab);
+
+    return newTab;
+}
+
+// axios call
+axios
+     .get("https://lambda-times-backend.herokuapp.com/topics")
+     .then(response => {
+         response.data.topics.forEach(data => {
+             tabTopics.append(newTopics(data))
+             console.log(response, 'response is working')
+         })
+     })
+     .catch(error => {
+         console.log('error here', error)
+     })
+
